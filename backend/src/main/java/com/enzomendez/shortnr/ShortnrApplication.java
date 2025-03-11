1f8b78cc77ddf40dc285f60fdddf3a655296fa1b
Enzo Mendez <u1373329@umail.utah.edu>
2025-03-11 15:44:02 -0600

    Add Spring Boot setup.

---
+++ backend/src/main/java/com/enzomendez/shortnr/ShortnrApplication.java
@@ -0,0 +1,13 @@
+package com.enzomendez.shortnr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortnrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortnrApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/enzomendez/shortnr/ShortnrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enzomendez.shortnr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortnrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
